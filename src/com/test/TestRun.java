98f389455d0c5b3aea5f4a72156dd109c5b27244
Barry Su <sai_1012@163.com>
2017-07-25 16:05:06 +0800

    Add again

---
+++ src/com/test/TestRun.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import com.roam.util.ChecksumUtil;
+
+public class TestRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChecksumUtil csu = new ChecksumUtil();
+		System.out.println(csu.doChecksumUns("D:/uns/DEV-45BT-010117-V013_Debug.uns"));
+	}
+
+}
